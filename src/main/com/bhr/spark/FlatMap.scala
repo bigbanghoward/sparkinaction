97eb7b46d78bd791c4ff26f82d66ec47b02beb39
bigbanghoward <bigbanghoward@163.com>
2016-04-28 16:56:06 +0800

    comit TopK

--- src/main/com/bhr/spark/FlatMap.scala
+++ src/main/com/bhr/spark/FlatMap.scala
@@ -5,22 +5,18 @@ import org.apache.spark._
 /**
   * Created by yesterday on 2016/4/27.
   */
-object TopK {
+object FlatMap {
 
   def main(args:Array[String]): Unit ={
     val conf = new SparkConf().setMaster("local").setAppName("wordcount")
     val sc = new SparkContext(conf)
-
     val textRdd = sc.textFile("data.txt")
-    println(textRdd)
 
-    println("rdd:" + textRdd.count)
+    println(textRdd)
 
     val wordCount = textRdd.flatMap {
       line => line.toString.split("\\s+")
-    }.map(word => (word,1)).reduceByKey{
-      (key,value) => key + value
-    }.collect()
+    }
 
     wordCount.foreach(println)
 
--- src/main/com/bhr/spark/Map.scala
+++ src/main/com/bhr/spark/Map.scala
@@ -5,24 +5,20 @@ import org.apache.spark._
 /**
   * Created by yesterday on 2016/4/27.
   */
-object TopK {
+object Map {
 
   def main(args:Array[String]): Unit ={
     val conf = new SparkConf().setMaster("local").setAppName("wordcount")
     val sc = new SparkContext(conf)
-
     val textRdd = sc.textFile("data.txt")
+
     println(textRdd)
 
-    println("rdd:" + textRdd.count)
-
-    val wordCount = textRdd.flatMap {
+    val wordCount = textRdd.map {
       line => line.toString.split("\\s+")
-    }.map(word => (word,1)).reduceByKey{
-      (key,value) => key + value
-    }.collect()
+    }
 
-    wordCount.foreach(println)
+    wordCount.foreach(_.foreach(println))
 
   }
 
